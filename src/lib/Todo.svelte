2b28b37c069fcdc53e2f15ef5a46dc2ba781572e
Mark Hansen <hansen-mark@hotmail.com>
2024-03-01 17:33:00 -0500

    Initialized Svelte.  Beginning drafting view of a task.

---
+++ src/lib/Todo.svelte
@@ -0,0 +1,37 @@
+<div class="todo-container">
+  <div class="priorities">
+    <div class="priority urgency">3</div>
+    <div class="priority importance">3</div>
+    <div class="priority time">5</div>
+  </div>
+  <div class="description">THIS IS MY DESCRIPTION</div>
+  <p>Tags:</p>
+  <ul>
+    <li>Tag1</li>
+    <li>Tag2</li>
+    <li>Tag3</li>
+  </ul>
+</div>
+
+<style>
+  .todo-container {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+    align-items: center;
+  }
+  .priorities{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+  }
+  .priority{
+    border: 1px solid black;
+    padding: 5px;
+    border-radius: 5px;
+  }
+</style>
